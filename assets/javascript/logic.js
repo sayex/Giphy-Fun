e137b82a2bad059dd0986561a9cf8633d359cfa5
sayex <elsayer@msn.com>
2018-12-07 18:46:03 -0700

    added dynamic element and click event to animate or still the img

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
 
-    // create varables to hold search and url
-    var apiKey = "3tQurqegTvlciDFPe3A3Wj1tX6KUQl99";
-    var search = ""
-    var url = "http://api.giphy.com/v1/gifs/search?q=" + search + "&;api_key=" + apiKey + "&limit=10";
+
 
 
     // create on click event to grab search box input
@@ -19,5 +16,60 @@ $(document).ready(function () {
         a.text(buttonVal);
         $(".jumbotron").append(a);
     })
+
+    //create function to search giphy and generate imgs
+
+    function giphySearch() {
+        $(".row").empty()
+        // create varables to hold search and url
+        var search = $(this).text()
+        var apiKey = "3tQurqegTvlciDFPe3A3Wj1tX6KUQl99";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=30";
+
+        // create ajax call to giphy
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            results = response.data;
+
+            for (i = 0; i < results.length; i++) {
+                newDiv = $("<div>");
+                newDiv.addClass("text-center p-1");
+                imgTag = $("<img>");
+                imgTag.attr("src", results[i].images.fixed_height_still.url)
+                imgTag.addClass("rounded")
+                imgTag.attr("data-state", "still")
+                imgTag.attr("data-still", results[i].images.fixed_height_still.url)
+                imgTag.attr("data-animate", results[i].images.fixed_height.url)
+                p = $("<p>").text("Rating " + results[i].rating)
+                newDiv.append(imgTag);
+                newDiv.append(p);
+                $(".row").append(newDiv);
+            }
+            $("img").on("click", function () {
+                imgState = $(this).attr("data-state");
+                imgStill = $(this).attr("data-still")
+                imgAnimate = $(this).attr("data-animate")
+                console.log(imgState);
+                if (imgState === "still") {
+                    $(this).attr("data-state", "animate");
+                    $(this).attr("src", imgAnimate)
+                } else if (imgState === "animate") {
+                    $(this).attr("data-state", "still");
+                    $(this).attr("src", imgStill);
+
+                }
+            })
+        })
+
+
+
+    }
+
+    $(document).on("click", ".search", giphySearch)
+
+
 
 })
